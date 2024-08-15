8394d05fb2cde14b34d7d88e6c55a988c1d269b7
Atiq Mansoori <er_asm@outlook.com>
2024-08-15 10:46:44 +0530

    ft. Extract transcript from YT video

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ load_dotenv()
 import google.generativeai as genai
 from youtube_transcript_api import YouTubeTranscriptApi
 
-gneai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 prompt = """Act as youtube video summarizer, Video transcript text will provide to you. So your task is to summarize entire video and return the important summary in points within 300 words. The transcript text will be appended here: 
 """
@@ -21,5 +21,17 @@ def transcript_text(url):
     
     try:
         video_id = url.split("=")[1]
-         
+        transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
+        
+        transcript=''
+        
+        for i in transcript_text:
+            transcript += ' '+i["text"]
+            
+        return transcript     
+        
+    except Exception as e:
+        raise e
+
+                 
 
---
+++ test.py
@@ -0,0 +1,7 @@
+from app import *
+
+transcript = ''
+
+transcript = transcript_text("https://www.youtube.com/watch?v=kU0wCDiqfew")
+
+print(transcript)
